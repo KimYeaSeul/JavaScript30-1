94e04744c46844a5acaa109a03ded58c3797901c
kimyeaseul <adgjl3151@hanmail.net>
2019-03-28 07:25:08 +0900

    Study : 190327

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,19 @@
+/* Get Our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viwer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* Build out functions */
+function togglePlay(){
+    console.log(video);
+    if(video.pause){
+        video.play();
+    }else{
+        video.pause();
+    }
+}
+/* Hook up the event listeners */
