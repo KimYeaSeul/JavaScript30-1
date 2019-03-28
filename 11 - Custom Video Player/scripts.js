e1793ed43e56d1bebedc11313e78750f230ed116
KimYeaSeul <adgjl3151@hanmail.net>
2019-03-28 17:32:32 +0900

    study : 190328

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,6 +1,6 @@
 /* Get Our Elements */
 const player = document.querySelector('.player');
-const video = player.querySelector('.viwer');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
@@ -9,11 +9,52 @@ const ranges = player.querySelectorAll('.player__slider');
 
 /* Build out functions */
 function togglePlay(){
-    console.log(video);
-    if(video.pause){
-        video.play();
-    }else{
-        video.pause();
-    }
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton(e){
+    const icon = e.paused ? '►' : '| |' ;
+    toggle.textContent = icon ;
+    // console.log('update the button');
+}
+
+function skip(){
+    // console.log(this); //다르게 표현할 수 있는 방법이 있는가
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+    video[this.name] = this.value;
+    console.log(this.name);
+    console.log(this.value);
+}
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+
+}
+
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
 }
 /* Hook up the event listeners */
+video.addEventListener('click', togglePlay); 
+video.addEventListener('play', updateButton); 
+video.addEventListener('pause', updateButton); 
+video.addEventListener('timeupdate', handleProgress); 
+toggle.addEventListener('click',togglePlay);
+skipButtons.forEach(button => button.addEventListener('click',skip));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', ()=> mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+//full screen
+//video.requestFullscreen()
+//document.exitFullscreen()
