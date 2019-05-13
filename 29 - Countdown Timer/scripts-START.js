b074924f3c95d4ba61536faa0498bd314a7fd222
KimYeaSeul <adgjl3151@hanmail.net>
2019-05-13 10:56:36 +0900

    modify: part-29

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,8 +1,11 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds){
+    clearInterval(countdown);
+
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
@@ -32,9 +35,23 @@ function displayTimeLeft(seconds) {
 
 function displayEndTime(timestamp){
     const end = new Date(timestamp);
-    const adjustedHour = hour > 12 ? hour-12 : hour; //시간을 12시간으로 표시하기 위한 변수
     const hour = end.getHours();
     const minutes = end.getMinutes();
-    endTime.textContent=`Be Back At ${adjustedHour}:${minutes}`;
+    const adjustedHour = hour > 12 ? hour-12 : hour; //시간을 12시간으로 표시하기 위한 변수
+    endTime.textContent=`Be Back At ${adjustedHour < 10 ? '0' : ''}${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
+function startTimer(){
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+    console.log(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(this);
+    timer(mins * 60);
+    this.reset(); //input을 비워줌
+});
